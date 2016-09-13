f6e101191b33df151654ca6558f8a242bcf63e4c
Eugenio Culurciello <euge@purdue.edu>
2016-09-13 11:46:08 -0400

    Update alexnetowtbn-small.lua
    updated sizes

--- models/alexnetowtbn-small.lua
+++ models/alexnetowtbn-small.lua
@@ -7,25 +7,28 @@ function createModel(nGPU)
    -- from https://code.google.com/p/cuda-convnet2/source/browse/layers/layers-imagenet-1gpu.cfg
    -- this is AlexNet that was presented in the One Weird Trick paper. http://arxiv.org/abs/1404.5997
    -- small version by Eugenio Culurciello, September 2016
+   
+   nn.SpatialMaxPooling:ceil() -- compatibility with Caffe and other frameworks!
+   
    local features = nn.Sequential()
    features:add(nn.SpatialConvolution(3,64,7,7,3,3))       -- 224 -> 73
    features:add(nn.SpatialBatchNormalization(64,1e-3))
    features:add(nn.ReLU(true))
    features:add(nn.SpatialMaxPooling(2,2,2,2))                   -- 73 ->  36
-   features:add(nn.SpatialConvolution(64,192,5,5,2,2))       --  36 -> 16
+   features:add(nn.SpatialConvolution(64,192,5,5,2,2))       --  36 -> 14
    features:add(nn.SpatialBatchNormalization(192,1e-3))
    features:add(nn.ReLU(true))
-   features:add(nn.SpatialMaxPooling(2,2,2,2))                   --  16 ->  8
-   features:add(nn.SpatialConvolution(192,384,3,3,1,1,1,1))      --  8 ->  8
+   features:add(nn.SpatialMaxPooling(2,2,2,2))                   --  14 ->  7
+   features:add(nn.SpatialConvolution(192,384,3,3,1,1,1,1))      --  7 ->  7
    features:add(nn.SpatialBatchNormalization(384,1e-3))
    features:add(nn.ReLU(true))
-   features:add(nn.SpatialConvolution(384,256,3,3,1,1,1,1))      --  8 ->  8
+   features:add(nn.SpatialConvolution(384,256,3,3,1,1,1,1))      --  7 ->  7
    features:add(nn.SpatialBatchNormalization(256,1e-3))
    features:add(nn.ReLU(true))
-   features:add(nn.SpatialConvolution(256,256,3,3,1,1,1,1))      --  8 ->  8
+   features:add(nn.SpatialConvolution(256,256,3,3,1,1,1,1))      --  7 ->  7
    features:add(nn.SpatialBatchNormalization(256,1e-3))
    features:add(nn.ReLU(true))
-   features:add(nn.SpatialAveragePooling(8, 8, 1, 1, 0, 0))      --  8 ->  1
+   features:add(nn.SpatialAveragePooling(7, 7, 1, 1, 0, 0))      --  7 ->  1
   
    features:cuda()
    features = makeDataParallel(features, nGPU) -- defined in util.lua
@@ -46,6 +49,6 @@ function createModel(nGPU)
 end
 
 -- test code:
--- local a = torch.FloatTensor(1,3,224,224)--:cuda() -- input image test
+-- local a = torch.FloatTensor(1,3,210,210)--:cuda() -- input image test
 -- local b = model:forward(a) -- test network
 -- print(b:size())
