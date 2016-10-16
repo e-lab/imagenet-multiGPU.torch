3d1368f25a4fd1aa2931a70833653db1eccac787
Eugenio Culurciello <euge@purdue.edu>
2016-10-16 16:24:39 +0100

    fixed model

--- models/alexnetowtbn-small.lua
+++ models/alexnetowtbn-small.lua
@@ -13,14 +13,15 @@ function createModel(nGPU)
    local features = nn.Sequential()
    features:add(nn.SpatialConvolution(3,64,7,7,3,3))       -- 224 -> 73
    features:add(nn.SpatialBatchNormalization(64,1e-3))
-   features:add(nn.ReLU(true))
    features:add(nn.SpatialMaxPooling(2,2,2,2):ceil())                   -- 73 ->  36
+   features:add(nn.ReLU(true))
    features:add(nn.SpatialConvolution(64,192,5,5,2,2))       --  36 -> 14
    features:add(nn.SpatialBatchNormalization(192,1e-3))
-   features:add(nn.ReLU(true))
    features:add(nn.SpatialMaxPooling(2,2,2,2):ceil())                   --  14 ->  7
+   features:add(nn.ReLU(true))
    features:add(nn.SpatialConvolution(192,384,3,3,1,1,1,1))      --  7 ->  7
    features:add(nn.SpatialBatchNormalization(384,1e-3))
+   features:add(nn.SpatialMaxPooling(2,2,2,2):ceil())                   --  14 ->  7
    features:add(nn.ReLU(true))
    features:add(nn.SpatialConvolution(384,256,3,3,1,1,1,1))      --  7 ->  7
    features:add(nn.SpatialBatchNormalization(256,1e-3))
@@ -28,7 +29,7 @@ function createModel(nGPU)
    features:add(nn.SpatialConvolution(256,256,3,3,1,1,1,1))      --  7 ->  7
    features:add(nn.SpatialBatchNormalization(256,1e-3))
    features:add(nn.ReLU(true))
-   features:add(nn.SpatialAveragePooling(7, 7, 1, 1, 0, 0))      --  7 ->  1
+   features:add(nn.SpatialAveragePooling(5, 5, 1, 1, 0, 0))      --  7 ->  1
   
    features:cuda()
    features = makeDataParallel(features, nGPU) -- defined in util.lua
@@ -43,12 +44,12 @@ function createModel(nGPU)
 
    local model = nn.Sequential():add(features):add(classifier)
    model.imageSize = 256
-   model.imageCrop = 210
+   model.imageCrop = 256
 
    return model
 end
 
 -- test code:
--- local a = torch.FloatTensor(1,3,210,210)--:cuda() -- input image test
+-- local a = torch.FloatTensor(1,3,256,256)--:cuda() -- input image test
 -- local b = model:forward(a) -- test network
 -- print(b:size())
